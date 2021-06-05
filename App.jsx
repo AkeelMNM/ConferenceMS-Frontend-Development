1869ababc6df2f1eb0ef1f4f06cade26464cd7eb
nushkymohamed <nuskymohamed1999@gmai.com>
2021-06-06 02:44:06 +0530

    All UI Created!Connection error,create error

--- App.jsx
+++ App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
 import Home from "./components/Home";
+
+
+import CreateConferenceComponent from './components/admin/CreateConferenceComponent';
+import EditConferenceComponent from './components/admin/EditConferenceComponent';
+import ListConferenceComponent from './components/admin/ListConferenceComponent';
+import EditorViewConferenceComponent from "./components/admin/EditorViewConferenceComponent";
+
 
 
 class App extends React.Component{
@@ -11,9 +18,11 @@ class App extends React.Component{
     render() {
         return <Router>
             <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
+                <Route path ="/" exact component ={ListConferenceComponent}></Route>
+                <Route path ="/add-conference/:id" component ={CreateConferenceComponent}></Route>
+                <Route path ="/view-conference/:id" component ={EditorViewConferenceComponent}></Route>
+                <Route exact path="/updateConference/:id" component={EditConferenceComponent}/>
+
             </Switch>
         </Router>
     }
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import CreateConferenceComponent from "./admin/CreateConferenceComponent";
 
 class Home extends React.Component{
     constructor(props) {
@@ -9,7 +10,16 @@ class Home extends React.Component{
 
     render() {
         return <div>
-            <h1>Welcome to Conference Management System</h1>
+            <Router>
+                <Switch>
+                    <Route exact="/"></Route>
+                    <Route path="/CreateConferenceComponent">
+                        <CreateConferenceComponent/>
+                    </Route>
+                </Switch>
+                <Link to={'/CreateConferenceComponent'}>Add</Link>
+            </Router>
+
         </div>
     }
 }
