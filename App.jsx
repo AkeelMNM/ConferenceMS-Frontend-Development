9cd36eac569c11a3a37588118e918b5a89747021
nushkymohamed <nuskymohamed1999@gmai.com>
2021-06-06 04:49:47 +0530

    All UI Created!Connection error

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
-import Home from "./components/Home";
+
 
 
 import CreateConferenceComponent from './components/admin/CreateConferenceComponent';
