3b794fc0f5877488648562b5693691013cdcbacb
Mohamed Akeel <akeel1447@gmail.com>
2021-06-30 15:47:08 +0530

    Minor change in Nav Bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ class Header extends React.Component{
         super(props);
 
         this.state = {
-            type:'Editor'
+            type:''
         }
     }
 
@@ -119,7 +119,7 @@ class Header extends React.Component{
                         <ul className={'menu'}>
                             <li><a href="/">Home</a></li>
                             <li style={{width:"250px"}}><a href="/display-workshops">Create Content</a></li>
-                            <li style={{width:"250px"}}><a href="/list-ContentView">View Contents</a></li>
+                            <li style={{width:"250px"}}><a href="/display-conference/:id">View Contents</a></li>
                             <li><a href="/About">Contact</a></li>
                         </ul>
                     </nav>
@@ -261,59 +261,6 @@ class Header extends React.Component{
                     (this.forAttendeeView())
                 :(this.forGuestView())
             }
-            {/*<div  id={'HeadDiv'}>
-                <label id={'HeadTitle'}>ICAF 2021</label>
-                <div id={'logRDiv'}>
-                    <a href={'#'} id={'regLink'}>Registration</a>
-                    <a href={'#'} id={'logLink'}>Login</a>
-                </div>
-
-            </div>
-            <div>
-                <div>
-                    <nav id={'bigDiv'}>
-
-                        <label htmlFor={'drop'} className={'toggle'}>Menu</label>
-                        <input type={'checkbox'} id={'drop'}/>
-                        <ul className={'menu'}>
-                            <li><a href="#">Home</a></li>
-                            <li>
-                                First Tier Drop Down
-                                <label htmlFor={'drop-1'} className={'toggle'}>Authors +</label>
-                                <a href="#">Authors</a>
-                                <input type={'checkbox'} id={'drop-1'}/>
-                                <ul>
-                                    <li><a href="#">Call For Papers</a></li>
-                                    <li><a href="#">Submission Instructions</a></li>
-                                </ul>
-
-                            </li>
-                            <li>
-
-                                 First Tier Drop Down
-                                <label htmlFor={'drop-2'} className={'toggle'}>Workshop +</label>
-                                <a href="#">Workshop</a>
-                                <input type={'checkbox'} id={'drop-2'}/>
-                                <ul>
-                                    <li><a href="#">Workshops</a></li>
-                                    <li><a href="#">Submission Instructions</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                First Tier Drop Down
-                                <label htmlFor={'drop-3'} className={'toggle'}>Attendees +</label>
-                                <a href="#">Attendees</a>
-                                <input type={'checkbox'} id={'drop-3'}/>
-
-                                <ul>
-                                    <li><a href="#">Registrations</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>*/}
         </div>
     }
 }
