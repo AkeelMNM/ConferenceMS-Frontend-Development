b3a20afc449fd844b08e3c63c15256d9830afc49
Mohamed Akeel <akeel1447@gmail.com>
2021-07-17 12:11:52 +0530

    Final Commit

--- components/admin/AdminDashboard.jsx
+++ components/admin/AdminDashboard.jsx
@@ -15,21 +15,25 @@ export default class AdminDashboard extends Component {
 
         }
 
-        this.go=this.go.bind(this);
+
     }
 
     componentDidMount() {
-        if(localStorage.getItem('_id') === null ){
+        if(localStorage.getItem('_id') === null && localStorage.getItem('type') !== 'Administrator'){
             this.props.history.push('/');
         }
     }
 
-    go(){
-        this.props.history.push('go/');
+
+
+    logout(event){
+        localStorage.clear();
+        this.props.history.push('/');
 
     }
     render() {
         return (<div>
+
                     <div className="nav-btn">Menu</div>
                     <div className="admin-container">
 
@@ -37,54 +41,68 @@ export default class AdminDashboard extends Component {
                             <div className="navDiv">
                                 <a className="aDLink" href="#"><span id="dashName">ICAF 2021</span></a>
                                 <ul id="dashUl">
-                                    <li className="dashLi active"><a className="aDLink" href="#">Dashboard</a></li>
+
+                                    <li className="dashLi"><a className="aDLink" href="/adminDashboard/">Dashboard</a></li>
                                     <li className="dashLi"><a className="aDLink" href="/adminCreateUser">Create User</a></li>
                                     <li className="dashLi"><a className="aDLink" href="/approve-conference/:id">View Requests</a></li>
-                                    <li className="dashLi"><a className="aDLink" href="/AdminView/">View Users</a></li>
+                                    <li className="dashLi"><a className="aDLink" href="/adminViewUser">View Users</a></li>
                                     <li className="dashLi"><a className="aDLink" href="/display-conference/:id">View Conference Contents</a></li>
+                                    <li className="dashLi"><a className="aDLink" href="/userProfile"> Admin Profile </a></li>
+
+
                                 </ul>
                             </div>
+                            <button id={'logOutAdmin'} onClick={event => this.logout(event)}>Log out</button>
                         </div>
 
 
                     </div>
 
                 <div className="main-content">
+
+
                     <h2>Admin Dashboard</h2>
-                    <p>ICAF 2021 Conferences</p>
+
+                    <p>ICAF 2021 SLIIT </p>
                     <div className="panel-wrapper">
                         <div className="panel-head">
-                            React js
+                            3RD INTERNATIONAL CONFERENCE ON ADVANCEMENTS IN COMPUTING 2021
                         </div>
                         <div className="panel-body">
                             Welcome to the 10th Chapter of ICAF 2021 International Conference on Application Frameworks for Sustainability 2021(IEEE ICIAFS 2021).
 
-                            The theme for the landmark chapter is “Endowing Intelligent Sustainability“ forcing on on artificial intelligence and machine learning based
-                            emerging technologies to improve and foster research in Sri Lanka and the region. As Industry 4.0 unfolds, computers are connected and
-                            communicate with one another to eventually make decisions without human participation. It includes cyber-physical systems, the Internet of
-                            things, cloud computing and cognitive computing.
+                            The 3rd International conference on advancements in computing -2021 (ICAC2021) is organized by the Faculty of Computing
+                            of the Sri Lanka Institute of Information Technology (SLIIT) as an open forum for academics along with
+                            industry professionals to present the latest findings and research output and practical deployments in the Computer Science and Information Technology domains.
+                            Primary objective of the ICAC is to uplift the research culture and the quality of research done by Sri Lankan researchers. This conference will create
+                            a platform for national and international researchers to showcase their research output, networking opportunities to discuss innovative ideas, and initiate collaborative work.
+                            ICAC 2019 and ICAC 2020 were successfully conducted with a technical co-sponsorship by IEEE Sri Lanka Section and all publications are available in IEEE Xplore digital library
+
+                            December 9 - 11 in Sri Lanka Institute of Information Technology
                         </div>
                     </div>
                     <div className="panel-wrapper">
                         <div className="panel-head">
-                            MERN Stack
+                            How to Write the Literature Review of your research paper
                         </div>
                         <div className="panel-body">
-                            Welcome to "The Complete MERN Stack Course 2021". In this course, we will build an in-depth Social Network website using Node.js, Express, React, and MongoDB along with ES6+. We will start with a blank text editor
-                            and end with a deployed full-stack website on Heroku.
-                            This course also uses the latest React Hooks and Redux which is an absolute joy. Your code will be so much simplified and readable.
-                            At the end of this course, we'll build an entire project(FACEBOOK CLONE) and you will learn how these different technologies work together step by step. We'll first have a look at all the individual building blocks so that we then can also combine them all into one amazing application by the end of the course.
+                            ICAC 2021 will be organizing a series of workshops which will be useful for potential authors and researchers on producing publications of significant quality.
+                            First workshop of the series titled “How to Write the Literature Review of your research paper”
+                            will be conducted by Prof. Chandimal Jayawardena, Dean/Faculty of Computing on 5th July 2021 from 4.00 p.m to 5.00 p.m
                         </div>
                     </div>
                     <div className="panel-wrapper">
                         <div className="panel-head">
-                            Koa js
+                            ICAC 2021 the next level of Research Initiative of  SLIIT
                         </div>
                         <div className="panel-body">
-                            In this semester (3YS1) we have a module named Application Frameworks. In order to pass this module,
-                            we need to build a group project using React JS. The project given to us was to build a web application for a Conference Management System. This system will mainly have 4 types of roles: Admin, Editor, Reviewer and User. Editors can add conference details and edit but Admin must approve them before they are published on the website.
-                            Registered users can submit research papers and workshop proposals. Reviewers can approve or reject them and then a notification will be sent to the relevant user.
-                            Guest Users can purchase tickets if they are willing to attend the conference.
+                            ICAC Conference was initiated by the Faculty of Computing, SLIIT and the first conference was held in December 2020.
+                            This year, the second conference will be organized for the second time with expanded scope and content.
+                            The 2nd International Conference on Advancements in
+                            Computing (ICAC 2021) will be held at the SLIIT on the 10th and 11th of December 2021.
+                            ICAC 2021 will be an excellent opportunity for researchers to be exposed to the world of innovations through
+                            research and to be able to showcase their talents and share their knowledge with the academia and the industry.
+
                         </div>
                     </div>
                 </div>
